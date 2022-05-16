d8da6ff438380eae5ba4721b801908515d8cbf6e
VCH <92458450+Inshanecr@users.noreply.github.com>
2022-05-16 11:40:53 +0430

    Add files via upload

---
+++ banner.py
@@ -0,0 +1,19 @@
+from time import sleep 
+from colorama import Fore
+y = Fore.GREEN
+ye = Fore.YELLOW
+re = Fore.RESET
+def banner():#banner
+  ban =(f"""
+¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤
+{ye}[2] >>{y} Show hacked camera
+                            
+{ye}[1] >>{y} URL shorter
+                              
+{ye}[0] >>{y} exit {re}
+¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤
+""")
+  for Line in ban.split("\n"):
+    sleep(0.1)
+    print(Line)
+          
